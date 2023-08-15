4c22f178181a3045509cedec0db1578c21882f40
L-Garay <garay.logan@gmail.com>
2023-08-15 14:02:53 -0600

    add new character icons

--- assets/svgs/index.ts
+++ assets/svgs/index.ts
@@ -1,2 +1,6 @@
 export { default as Cloud } from './Cloud';
 export { default as CloudReversed } from './CloudReversed';
+export { default as Zombie } from './Zombie';
+export { default as Ghost } from './Ghost';
+export { default as Vampire } from './Vampire';
+export { default as Mummy } from './Mummy';
