595b17f63cca72d5520d747f3a0a69ad10924f93
L-Garay <garay.logan@gmail.com>
2023-08-17 11:31:07 -0600

    add settings wheel svg to project

--- assets/svgs/index.ts
+++ assets/svgs/index.ts
@@ -4,3 +4,4 @@ export { default as Zombie } from './Zombie';
 export { default as Ghost } from './Ghost';
 export { default as Vampire } from './Vampire';
 export { default as Mummy } from './Mummy';
+export { default as SettingsWheel } from './SettingsWheel';
