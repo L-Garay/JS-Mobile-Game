836a664fa11397e55ea3be26854c5c1c33ff765f
L-Garay <garay.logan@gmail.com>
2023-08-31 11:05:15 -0600

    make seperate component for settings wheel and modal

--- app/game-center/GAME1.tsx
+++ app/game-center/GAME1.tsx
@@ -1,14 +1,12 @@
-import useCharacterContext from '../../contexts/CharacterContext';
-import { SettingsWheel } from '../../assets/svgs';
-import BasicModal from '../../components/Modals/BasicModal';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { useNavigation } from 'expo-router';
-import React, { useEffect, useState } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import useCharacterContext from '../../contexts/CharacterContext';
+import SettingsWheelModal from '../../components/GameCenter/SettingsWheelModal';
 
 export default function GAME1() {
   const navigation = useNavigation();
   const { currentCharacter } = useCharacterContext();
-  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
   // IMO this should catch if there is no currentCharacter
   // and therefore I feel comfortable using the '!' operator
@@ -20,27 +18,7 @@ export default function GAME1() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.settingsWheel}>
-        <Pressable onPress={() => setIsModalVisible(prev => !prev)}>
-          <SettingsWheel />
-        </Pressable>
-      </View>
-
-      <BasicModal
-        isModalVisible={isModalVisible}
-        setIsModalVisible={setIsModalVisible}
-        message="This modal is for the game center"
-        buttons={[
-          { text: 'Home', action: () => navigation.navigate('index') },
-          {
-            text: 'Game Center',
-            action: () =>
-              navigation.navigate('game-center/index', {
-                characterName: currentCharacter!.name
-              })
-          }
-        ]}
-      />
+      <SettingsWheelModal />
       <Text style={styles.title}>GAME1</Text>
     </View>
   );
@@ -56,12 +34,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#d6a811',
     alignSelf: 'center'
-  },
-  settingsWheel: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    padding: 20,
-    paddingLeft: 25
   }
 });
